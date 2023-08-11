701dd88ae5c564884806aa3de01b04646acfba97
T-clown <30548891+T-clown@users.noreply.github.com>
2023-08-11 19:07:15 +0800

    Delete api-gateway directory

--- api-gateway/gateway-admin/src/main/java/com/api/gateway/mapper/PluginMapper.java
+++
@@ -1,9 +0,0 @@
-package com.api.gateway.mapper;
-
-import com.api.gateway.model.entity.Plugin;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-
-public interface PluginMapper extends BaseMapper<Plugin> {
-
-}
